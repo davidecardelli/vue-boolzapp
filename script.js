a48cae8e9843f0d19f8551d4316e4b37c79fe374
davidecardelli <davide.cardelli.94@gmail.com>
2023-01-13 09:26:46 +0100

    add v-model for newMessage

--- script.js
+++ script.js
@@ -6,14 +6,18 @@ const app = Vue.createApp({
         return {
          currentIndex: 0,
          data,
+         newMessage: '',
         }
     },
 
     methods: {
+        
+        // Cambio del currentIndex che userò per girare nella lista delle conversazioni. 
         goTo(index) {
            this.currentIndex = index;
         },
-
+        
+        // Ternario che mi risponde se il messaggio è stato ricevuto o meno.
         isRecived(i){
             return this.data.contacts[this.currentIndex].messages[i].status === 'received'
         }
